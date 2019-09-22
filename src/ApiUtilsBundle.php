ebbe80d4b13310d98cdc2637fee9afe58c9bb812
Warren Heyneke <45089714+prof83@users.noreply.github.com>
2019-09-22 11:36:06 +0200

    Initial

---
+++ src/ApiUtilsBundle.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * This file is part of the Bit 64 API Utility Bundle for Symfony
+ *
+ * Copyright © 2019 Bit 64 Solutions (Pty) Ltd <hello@bit64.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+namespace Bit64\Bundle\ApiUtilsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Warren Heyneke <hello@bit64.co>
+ */
+class ApiUtilsBundle extends Bundle {
+
+}
---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,55 @@
+<?php
+/*
+ * This file is part of the Bit 64 API Utility Bundle for Symfony
+ *
+ * Copyright © 2019 Bit 64 Solutions (Pty) Ltd <hello@bit64.co>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+*/
+
+namespace Bit64\Bundle\ApiUtilsBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * @author Warren Heyneke <hello@bit64.co>
+ */
+class Configuration implements ConfigurationInterface {
+
+	public function getConfigTreeBuilder() {
+
+		$treeBuilder = new TreeBuilder;
+		$node = $treeBuilder->root('bit64_api_utils');
+
+		$node
+			->children()
+				->arrayNode('view_handlers')
+					->info('Available view resolvers for controller results')
+					->isRequired()
+					->useAttributeAsKey()
+					->children()
+						->booleanNode('json_array')
+							->isRequired()
+							->defaultValue(true)
+							->info('Resolve native arrays as a JsonResponse')
+						->end()
+						->booleanNode('json_object')
+							->isRequired()
+							->defaultValue(true)
+							->info('Resolve instances of JsonSerializable as a JsonResponse')
+						->end()
+						->booleanNode('json_form_errors')
+							->isRequired()
+							->defaultValue(true)
+							->info('Extract all FormError objects from a Symfony FormInterface and resolve as a JsonResponse')
+						->end()
+					->end()
+				->end()
+			->end();
+
+		return $treeBuilder;
+	}
+
+}
